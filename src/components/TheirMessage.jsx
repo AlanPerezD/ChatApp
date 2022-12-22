9b6a844b8522ffb59edfd1eabba826da2ed24d12
AlanPerezD <alanperezd@outlook.es>
2022-12-22 15:18:33 -0300

    added components

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username != message.sender.username;
+
   return (
-    <div>
-      TheirMessage
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div 
+          className="message-avatar"
+          style={{ backgrounImage: `url(${message?.sender?.avatar})`}}
+        />
+      )}
     </div>
   )
 }
