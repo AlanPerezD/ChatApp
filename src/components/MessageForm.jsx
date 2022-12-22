0b3b9c680ee4a39570880d9d37b642ffa4650f29
AlanPerezD <alanperezd@outlook.es>
2022-12-22 19:49:00 +0000

    fixed typos, added chat feed, messageform, mymessage and Theirmessage

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,39 @@
-const MessageForm = () => {
+import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+
+const MessageForm = (props) => {
+  const [ value, setValue ] = useState('');
+  const { chatId, creds } = props;
+
+  const handleChange = ( event ) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
+
+  const handleSubmit = ( event ) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if(text.length > 0 ) {
+      sendMessage(creds, chatId, { text })
+    }
+
+    setValue('');
+  };
+
+
   return (
-    <div>
-      MessageForm
-    </div>
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input 
+        className="message-input"
+        placeholder="Send a message ..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </form>
   )
 }
 
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MyMessage = ({ message }) => {
-  if (message?.attachments?.length > 0) {
+  if (message.attachments && message.attachments.length > 0) {
     return (
       <img 
         src={message.attachments[0].file}
@@ -12,9 +12,8 @@ const MyMessage = ({ message }) => {
     )
   }
   return (
-    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A5'}}>
-      
-      {MyMessage.text}
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'black', backgroundColor: '#3B2A5'}}>
+      {message.text}
     </div>
   )
 }
